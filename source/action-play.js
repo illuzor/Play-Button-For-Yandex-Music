20a8665c17b9b10e64bd031cdc8195113093fd65
Artem Zanin <illuzor@gmail.com>
2021-04-21 21:43:21 +0300

    attempt to fix background play with delay

--- source/action-play.js
+++ source/action-play.js
@@ -1,14 +1,39 @@
 ﻿var icon = "Play";
 
 var playButtons = document.getElementsByClassName("player-controls__btn deco-player-controls__button player-controls__btn_play");
-var isPlayingNow = document.getElementsByClassName("player-controls__btn deco-player-controls__button player-controls__btn_play player-controls__btn_pause").length > 0;
 
-if (playButtons.length != 0) {
-    playButtons[playButtons.length - 1].click();
+var isPlaying = isPlayingNow();
+var needAdditionalClick = !isPlaying;
+var additionalClickPerformed = false;
+
+if (!isPlaying) {
+    icon = "Pause";
 }
 
-if (!isPlayingNow) {
-    icon = "Pause";
+if (playButtons.length != 0) {
+    click();
+}
+
+function click() {
+    console.log("click(). isPlayingNow:" + isPlayingNow() + " needAdditionalClick:" + needAdditionalClick);
+    playButtons[playButtons.length - 1].click();
+
+    if (needAdditionalClick && !additionalClickPerformed) {
+        setTimeout(clickAgain, 150);
+    }
+}
+
+function clickAgain() {
+    console.log("clickAgain(). isPlayingNow:" + isPlayingNow() + " needAdditionalClick:" + needAdditionalClick + " clickedAgain:" + additionalClickPerformed);
+    if (!isPlayingNow()) {
+        console.log("clickAgain(). clicked again");
+        additionalClickPerformed = true;
+        click();
+    }
+}
+
+function isPlayingNow() {
+    return document.getElementsByClassName("player-controls__btn deco-player-controls__button player-controls__btn_play player-controls__btn_pause").length > 0;
 }
 
 icon;
